4923e630622729b58b5d498cf5fb2f30d585c600
Matt Ruetz <91920147+MattRuetz@users.noreply.github.com>
2022-03-11 20:08:45 -0500

    update index.tsx

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/darkly/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-//import CodeCell from "./components/code-cell";
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
